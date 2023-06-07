7680b3625b1a22ada1e39b075781cb9fcde8ad68
Sakib669 <stewieboyyy@gmail.com>
2023-06-07 14:59:16 -0700

    authentication and auth provider added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAmkPn7kpn8qFsW-rhTPzLsTtGQXYgIHho",
+  authDomain: "summer-camp-client-ef842.firebaseapp.com",
+  projectId: "summer-camp-client-ef842",
+  storageBucket: "summer-camp-client-ef842.appspot.com",
+  messagingSenderId: "308180894242",
+  appId: "1:308180894242:web:a3d6c1b5be7c59a457807b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import AuthProvider from './Providers/AuthProvider.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    children: [
+      {
+
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>,
 )
